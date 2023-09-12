83be93699077d166e5066069ba6bb8f9575f8221
MaxBuster380 <maxbuster380@gmail.com>
2023-09-12 12:33:42 +0200

    Added distanceCheck2 test to HexagonVectorTest

--- src/test/kotlin/HexagonVectorTest.kt
+++ src/test/kotlin/HexagonVectorTest.kt
@@ -1,7 +1,6 @@
 import kotlin.math.abs
 import kotlin.random.Random
 import kotlin.test.Test
-import HexagonVector
 import kotlin.test.assertEquals
 
 class HexagonVectorTest {
@@ -9,6 +8,9 @@ class HexagonVectorTest {
         const val RANDOM_CHECK_COUNT = 1e3.toInt()
     }
 
+    /**
+     * Check for HOR_RIGHT * a + ASC_RIGHT * b
+     */
     @Test
     fun distanceCheck1() {
         for(i in 1..RANDOM_CHECK_COUNT) {
@@ -16,6 +18,24 @@ class HexagonVectorTest {
             val bShift = (abs(Random.nextInt()) % 1E6).toInt()
 
             val hexagon = HexagonVector.HOR_RIGHT * aShift + HexagonVector.ASC_RIGHT * bShift
+
+            assertEquals(
+                hexagon.distance(HexagonVector.ORIGIN),
+                aShift + bShift
+            )
+        }
+    }
+
+    /**
+     * Check for HOR_RIGHT * a + DESC_RIGHT * b
+     */
+    @Test
+    fun distanceCheck2() {
+        for(i in 1..RANDOM_CHECK_COUNT) {
+            val aShift = (abs(Random.nextInt()) % 1E6).toInt()
+            val bShift = (abs(Random.nextInt()) % 1E6).toInt()
+
+            val hexagon = HexagonVector.HOR_RIGHT * aShift + HexagonVector.DESC_RIGHT * bShift
 
             assertEquals(
                 hexagon.distance(HexagonVector.ORIGIN),
